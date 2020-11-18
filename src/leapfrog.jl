bd08732ab3777771ac8789221b9da7fdf92ea28c
GiggleLiu <cacate0129@gmail.com>
2020-11-17 19:30:15 -0500

    shorten the code

--- src/leapfrog.jl
+++ src/leapfrog.jl
@@ -18,29 +18,38 @@ function leapfrog(planets::AbstractVector{Body{T}}; G=G_year_AU, n, dt, keep_his
         v[:] .= getfield.(planets, :v)
         r[:] .= getfield.(planets, :r)
     end
-    leapfrog!(r, v, planets; G=G, n=n, dt=dt)
+    m = getfield.(planets, :m)
+    leapfrog!(r, v, m; G=G, n=n, dt=dt)
 end
 
-function leapfrog!(r::AbstractVecOrMat, v::AbstractVecOrMat,
-         planets::AbstractVector{Body{T}}; G, n, dt) where T
-    nplanets = length(planets)
+function leapfrog!(r::AbstractVecOrMat{V3{T}}, v::AbstractVecOrMat{V3{T}},
+         m::AbstractVector; G, n, dt) where T
+    nplanets = length(m)
     a = zeros(V3{T}, nplanets)
     @inbounds for i=1:n
         idx = r isa AbstractVector ? 1 : i
         idx_plus1 = r isa AbstractVector ? 1 : i+1
-        for j=1:nplanets
-            # compute acceleration
-            a[j] = zero(V3{T})
-            for k=1:nplanets
-                if j != k
-                    a[j] += acceleration(r[j,idx], r[k,idx], planets[k].m, G)
-                end
-            end
-        end
+        update_acceleration!(a, _view(r,:,idx), m, G)
         for j=1:nplanets
             v[j,idx_plus1] = (i==1 ? dt/2 : dt)*a[j] + v[j,idx]
             r[j,idx_plus1] = dt*v[j,idx_plus1] + r[j,idx]
         end
     end
     return r, v
+end
+
+_view(a, i, j) = view(a, i, j)
+_view(a::AbstractVector, args...) = a
+
+function update_acceleration!(a::AbstractVector{V3{T}}, r, m, G) where T
+    @assert length(a) == length(m)
+    @assert length(a) == length(r)
+    @inbounds for j=1:length(m)
+        a[j] = zero(V3{T})
+        for k=1:length(m)
+            if j != k
+                a[j] += acceleration(r[j], r[k], m[k], G)
+            end
+        end
+    end
 end
--- src/pefrl.jl
+++ src/pefrl.jl
@@ -4,15 +4,16 @@
 function fr!(r::AbstractVecOrMat, v::AbstractVecOrMat, a::AbstractVecOrMat,
          planets::AbstractVector{Body{T}}; G, n, dt) where T
     nplanets = length(planets)
+    a = zeros(V3{T}, nplanets)
     @inbounds for i=1:n
         idx = r isa AbstractVector ? 1 : i
         idx_plus1 = r isa AbstractVector ? 1 : i+1
         # compute acceleration
         for j=1:nplanets
-            a[j,idx] = zero(V3{T})
+            a[j] = zero(V3{T})
             for k=1:nplanets
                 if j != k
-                    a[j,idx] += acceleration(r[j,idx], r[k,idx], planets[k].m, G)
+                    a[j] += acceleration(r[j,idx], r[k,idx], planets[k].m, G)
                 end
             end
         end
@@ -28,6 +29,8 @@ function fr!(r::AbstractVecOrMat, v::AbstractVecOrMat, a::AbstractVecOrMat,
                 r[j,idx_plus1] = r[j,idx]
                 v[j,idx_plus1] = v[j,idx]
                 r[j,idx_plus1] += θ*dt/2*v[j,idx_plus1]
+            end
+            # compute acceleration
                 v[j,idx_plus1] += θ*dt*a[j]
             end
         end
